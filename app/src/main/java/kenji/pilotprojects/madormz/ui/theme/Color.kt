edf3cf3397375c8715d983fab128b9209542a0b2
lusciousmemo <keng.lusciousmemo@gmail.com>
2023-05-22 16:33:15 +0700

    :tada: Init Project to Github
    Project's first commit

---
+++ app/src/main/java/kenji/pilotprojects/madormz/ui/theme/Color.kt
@@ -0,0 +1,14 @@
+package kenji.pilotprojects.madormz.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple200 = Color(0xFFBB86FC)
+val Purple500 = Color(0xFF6200EE)
+val Purple700 = Color(0xFF3700B3)
+val Teal200 = Color(0xFF03DAC5)
+
+val madromz1 = Color(0xFFBDBDBD)
+val madromz2 = Color(0xFF263238)
+val madromz3 = Color(0xFF5C6BC0)
+val madromz4 = Color(0xFF6A1B9A)
+val madromz5 = Color(0xFF616161)
